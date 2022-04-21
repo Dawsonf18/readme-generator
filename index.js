ca0257d0ea974df335ee3a96b995910b57c3689c
Dawson Filion <Dawsonfilion21@gmail.com>
2022-04-21 15:38:46 -0400

    changed index js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// require modules 
 const fs = require('fs'); 
-const inquirer = require('inquire'); 
+const inquirer = require('inquirer'); 
 
 // linking to page where the README is developed 
 const generatePage = require('./utils/generateMarkdown.js');
